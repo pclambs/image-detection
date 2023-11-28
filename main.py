d5526e5f7147946b9092d32de22c03483edd8f0a
deepwubs <pchucklambs@gmail.com>
2023-11-27 22:54:04 -0600

    detect matches in multiple locations

--- main.py
+++ main.py
@@ -1,41 +1,39 @@
 import cv2 as cv
 import numpy as np
+import os
+
+# change working directory to the folder this script is in
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # load images
 haystack_img = cv.imread('mine.png', cv.IMREAD_UNCHANGED)
 needle_img = cv.imread('copper.png', cv.IMREAD_UNCHANGED)
 
 result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
+print(result)
 
-# get the best match position
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
+threshold = 0.39
+locations = np.where(result >= threshold)
+# zip will create tuples of x, y coordinates
+locations = list(zip(*locations[::-1]))
+# print(locations)
 
-print('Best match top left position: %s' % str(max_loc))
-print('Best match confidence: %s' % max_val)
+if locations:
+  print('Found needle.')
 
-# threshold to decide when we have found a match
-threshold = 0.8
-if max_val >= threshold:
-  print('found needle')
-
-  # get dimensions of the needle image
   needle_w = needle_img.shape[1]
   needle_h = needle_img.shape[0]
+  line_color = (0, 255, 0)
+  line_type = cv.LINE_4
 
-  # get dimensions of the needle image
-  top_left = max_loc
-  bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+  # loop over all the locations and draw their rectangles
+  for loc in locations:
+    # determine the box positions
+    top_left = loc
+    bottom_right = (top_left[0] + needle_w, top_left[1] + needle_h)
+    # draw the box
+    cv.rectangle(haystack_img, top_left, bottom_right, line_color, line_type)
 
-  # draw a rectangle around the matched region
-  cv.rectangle(haystack_img, top_left, bottom_right, 
-                color=(0, 255, 0), thickness=2, lineType=cv.LINE_4)
-  
-  # show the result
-  # cv.imshow('Result', haystack_img)
-
-  # save the result
-  cv.imwrite('result.png', haystack_img)
-
+  cv.imshow('Matches', haystack_img)
   cv.waitKey()
-else:
-  print('no needle found')
+  cv.imwrite('result.png', haystack_img)
